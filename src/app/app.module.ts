bb95814b98efd61dd958e385cd2595fcaaa40782
kiku98 <info@kiliankunkel.de>
2024-03-07 15:36:25 -0500

    init user manager

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContentComponent } from './content/content.component';
 import { HeaderComponent } from './header/header.component';
+import { UserManagerComponent } from './header/user-manager/user-manager.component';
 
 @NgModule({
-  declarations: [AppComponent, ContentComponent, HeaderComponent],
+  declarations: [AppComponent, ContentComponent, HeaderComponent, UserManagerComponent],
   imports: [BrowserModule, AppRoutingModule, MatToolbarModule, MatIconModule],
   providers: [],
   bootstrap: [AppComponent],
